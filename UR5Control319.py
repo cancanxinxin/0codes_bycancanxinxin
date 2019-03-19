f5f56c00abf119ed8ad55bb6430aa61ee0f695f0
cancanxinxin <516649919@qq.com>
2019-03-19 11:05:04 +0800

    Add files via upload
    UR5 在Vrep中基本的仿真控制

---
+++ UR5Control319.py
@@ -0,0 +1,84 @@
+from __future__ import division
+import numpy as np
+import math
+import vrep
+
+# Step1 Setting
+
+RAD2DEG = 180 / math.pi   # 常数，弧度转度数
+tstep = 0.005             # 定义仿真步长
+# 配置关节信息
+jointNum = 6
+baseName = 'UR5'
+jointName = 'UR5_joint'
+
+# Step2 Init
+print('Program started')
+# 关闭潜在的连接
+vrep.simxFinish(-1)
+# 每隔0.2s检测一次，直到连接上V-rep
+while True:
+    clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+    if clientID > -1:
+        break
+    else:
+        time.sleep(0.2)
+        print("Failed connecting to remote API server!")
+print("Connection success!")
+
+# Step3 Configuration
+# 设置仿真步长，为了保持API端与V-rep端相同步长
+vrep.simxSetFloatingParameter(clientID, vrep.sim_floatparam_simulation_time_step, tstep, vrep.simx_opmode_oneshot)
+# 然后打开同步模式
+vrep.simxSynchronous(clientID, True) 
+vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+
+# 然后读取Base和Joint的句柄
+jointHandle = np.zeros((jointNum,), dtype=np.int) # 注意是整型
+for i in range(jointNum):
+    _, returnHandle = vrep.simxGetObjectHandle(clientID, jointName + str(i+1), vrep.simx_opmode_blocking)
+    jointHandle[i] = returnHandle
+
+_, baseHandle = vrep.simxGetObjectHandle(clientID, baseName, vrep.simx_opmode_blocking)
+
+print('Handles available!')
+
+# 然后首次读取关节的初始值，以streaming的形式
+jointConfig = np.zeros((jointNum,))
+for i in range(jointNum):
+     _, jpos = vrep.simxGetJointPosition(clientID, jointHandle[i], vrep.simx_opmode_streaming)
+     jointConfig[i] = jpos
+
+# Step4 Simulation
+lastCmdTime=vrep.simxGetLastCmdTime(clientID)  # 记录当前时间
+vrep.simxSynchronousTrigger(clientID)  # 让仿真走一步
+
+# 开始仿真
+while vrep.simxGetConnectionId(clientID) != -1:
+    currCmdTime=vrep.simxGetLastCmdTime(clientID)  # 记录当前时间
+    dt = currCmdTime - lastCmdTime # 记录时间间隔，用于控制
+    # ***
+    # CoreControl
+    # 读取当前的状态值，之后都用buffer形式读取
+    for i in range(jointNum):
+        _, jpos = vrep.simxGetJointPosition(clientID, jointHandle[i], vrep.simx_opmode_buffer)
+        print(round(jpos * RAD2DEG, 2))
+        jointConfig[i] = jpos
+
+    # 控制命令需要同时方式，故暂停通信，用于存储所有控制命令一起发送
+    vrep.simxPauseCommunication(clientID, True)
+    start1 = [-1.57, 0.06, -1.14, 1.06, 1.57, 0.00]
+    # start1 = input()
+    if start1 == -1:
+        break
+    for i in range(jointNum):
+        # vrep.simxSetJointTargetPosition(clientID, jointHandle[i], 120/RAD2DEG, vrep.simx_opmode_oneshot)    #original
+        vrep.simxSetJointTargetPosition(clientID, jointHandle[i], start1[i], vrep.simx_opmode_oneshot)
+        vrep.simxPauseCommunication(clientID, False)
+
+    lastCmdTime=currCmdTime    # 记录当前时间
+    vrep.simxSynchronousTrigger(clientID)  # 进行下一步
+    vrep.simxGetPingTime(clientID)    # 使得该仿真步走完
+
+# Step5 CoreControl from the code position #***
+
