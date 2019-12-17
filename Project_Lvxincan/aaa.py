29e0b70bc90ec0e9ae6dcaff8c2f093b0f5d7399
cancanxinxin <516649919@qq.com>
2019-12-17 10:04:21 +0800

    Create aaa.py

---
+++ Project_Lvxincan/aaa.py
@@ -0,0 +1,12 @@
+def print1_1():
+	print("this is the test")
+
+import cv2 as cv
+
+# # help(cv.resize)
+# img = cv.imread(frame12.jpg)
+# cv.resize(img,(1920,1080), interpolation=cv.INTER_CUBIC)
+help(cv.solvePnP)
+# import platform 
+# print(platform.python_version())
+# print(cv.__version__)
